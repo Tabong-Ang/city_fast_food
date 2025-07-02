014cbbdca7c30d04e5b416720306d07da24c3c25
Tabong-Ang <tabongphilip@gmail.com>
2025-07-02 20:16:31 +0100

    updated files

--- deliver/deliver/settings.py
+++ deliver/deliver/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(4pn@4x%c7f#9dh7&c^s@kf7vjwh7wy_w(5-4-2k3^yw+8wlx*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
