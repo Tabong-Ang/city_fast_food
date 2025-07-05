882279dbc1bbb9754417a90b537f930fc143b8fb
Tabong-Ang <tabongphilip@gmail.com>
2025-07-05 13:48:15 +0100

    improved UX/UI

--- deliver/deliver/settings.py
+++ deliver/deliver/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-(4pn@4x%c7f#9dh7&c^s@kf7vjwh7wy_w(5-4-2k3^yw+8wlx*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
